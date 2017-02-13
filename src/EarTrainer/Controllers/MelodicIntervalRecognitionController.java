23787cbe7d0fd626c06eae9600c17fd07724a0d0
Timan Noel <tnoel7@msn.com>
2017-02-13 11:44:35 +0000

    Added answer buttons and radio button group to Melodic Interval Recognition page

--- src/EarTrainer/Controllers/MelodicIntervalRecognitionController.java
+++ src/EarTrainer/Controllers/MelodicIntervalRecognitionController.java
@@ -1,7 +1,10 @@
 package EarTrainer.Controllers;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -10,6 +13,12 @@ import java.io.IOException;
 public class MelodicIntervalRecognitionController {
 
     @FXML private StackPane stackPane;
+
+    @FXML private ToggleGroup radioButtons;
+    @FXML private RadioButton easyRadioButton;
+    @FXML private RadioButton mediumRadioButton;
+    @FXML private RadioButton hardRadioButton;
+
 
     @FXML
     private void BackButtonClicked(ActionEvent event) throws IOException {
